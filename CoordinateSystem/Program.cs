aaaf16a39b9f227897e3e946355992a35893bdd6
Valery Kardel <kardelw@gmail.com>
2019-04-23 20:06:56 +0300

    Добавьте файлы проекта.

---
+++ CoordinateSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoordinateSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo keyInfo;
+            Console.WriteLine("Press key 'X' to quit");
+            while (true)
+            {
+                keyInfo = Console.ReadKey(true);
+                // Exit if the user pressed the 'X' key.
+                if (keyInfo.Key == ConsoleKey.X) break;
+                var console = new ConsoleDisplay();
+                console.OutputPoint(console.InputPoint());
+            }
+
+        }
+    }
+}
